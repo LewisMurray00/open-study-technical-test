d0df0138896b76f372a3106234fca952adcce8e3
Lewis Murray <lewisdmurray00@gmail.com>
2022-06-07 20:28:05 +0100

    Installed React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import ShoppingBasket from './Pages/shoppingBasket/ShoppingBasket';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="shopping-basket" exact element={<ShoppingBasket />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/Pages/shoppingBasket/ShoppingBasket.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShoppingBasket = () => {
+  return (
+    <div>Your basket is empty</div>
+  )
+}
+
+export default ShoppingBasket
